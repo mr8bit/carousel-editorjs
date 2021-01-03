3cd493a592764a894367e3039b0870ce3a99c184
Artemiy <a.mazaew@lambda-it.ru>
2021-01-03 18:28:23 +0300

    Rename tag carousel to cdxcarousel

--- src/index.js
+++ src/index.js
@@ -48,14 +48,14 @@ export default class SimpleCarousel {
       /**
        * Tool's classes
        */
-      wrapper: 'carousel-wrapper',
-      addButton: 'carousel-addImage',
-      block: 'carousel-block',
-      item: 'carousel-item',
-      removeBtn: 'carousel-removeBtn',
-      inputUrl: 'carousel-inputUrl',
-      caption: 'carousel-caption',
-      list: 'carousel-list',
+      wrapper: 'cdxcarousel-wrapper',
+      addButton: 'cdxcarousel-addImage',
+      block: 'cdxcarousel-block',
+      item: 'cdxcarousel-item',
+      removeBtn: 'cdxcarousel-removeBtn',
+      inputUrl: 'cdxcarousel-inputUrl',
+      caption: 'cdxcarousel-caption',
+      list: 'cdxcarousel-list',
       imagePreloader: 'image-tool__image-preloader'
     };
   };
@@ -70,7 +70,7 @@ export default class SimpleCarousel {
   static get toolbox() {
     return {
       title: 'Carousel',
-      icon: '<svg width="38" height="18" viewBox="0 0 38 18" fill="none" xmlns="http://www.w3.org/2000/svg"><mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="10" y="0" width="18" height="18"><path fill-rule="evenodd" clip-rule="evenodd" d="M28 16V2C28 0.9 27.1 0 26 0H12C10.9 0 10 0.9 10 2V16C10 17.1 10.9 18 12 18H26C27.1 18 28 17.1 28 16V16ZM15.5 10.5L18 13.51L21.5 9L26 15H12L15.5 10.5V10.5Z" fill="white"/></mask><g mask="url(#mask0)"><rect x="10" width="18" height="18" fill="#707684"/></g><mask id="mask1" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="3" width="7" height="12"><path fill-rule="evenodd" clip-rule="evenodd" d="M7 13.59L2.67341 9L7 4.41L5.66802 3L0 9L5.66802 15L7 13.59Z" fill="white"/></mask><g mask="url(#mask1)"><rect y="3" width="7.55735" height="12" fill="#707684"/></g><mask id="mask2" mask-type="alpha" maskUnits="userSpaceOnUse" x="31" y="3" width="7" height="12"><path fill-rule="evenodd" clip-rule="evenodd" d="M31 13.59L35.3266 9L31 4.41L32.332 3L38 9L32.332 15L31 13.59Z" fill="white"/></mask><g mask="url(#mask2)"><rect x="30.4426" y="2.25" width="7.55735" height="13" fill="#707684"/></g></svg>'
+      icon: '<svg width="38" height="18" viewBox="0 0 38 18" fill="none" xmlns="http://www.w3.org/2000/svg"><mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="10" y="0" width="18" height="18"><path fill-rule="evenodd" clip-rule="evenodd" d="M28 16V2C28 0.9 27.1 0 26 0H12C10.9 0 10 0.9 10 2V16C10 17.1 10.9 18 12 18H26C27.1 18 28 17.1 28 16V16ZM15.5 10.5L18 13.51L21.5 9L26 15H12L15.5 10.5V10.5Z"  /></mask><g mask="url(#mask0)"><rect x="10" width="18" height="18"  /></g><mask id="mask1" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="3" width="7" height="12"><path fill-rule="evenodd" clip-rule="evenodd" d="M7 13.59L2.67341 9L7 4.41L5.66802 3L0 9L5.66802 15L7 13.59Z" fill="white"/></mask><g mask="url(#mask1)"><rect y="3" width="7.55735" height="12"  /></g><mask id="mask2" mask-type="alpha" maskUnits="userSpaceOnUse" x="31" y="3" width="7" height="12"><path fill-rule="evenodd" clip-rule="evenodd" d="M31 13.59L35.3266 9L31 4.41L32.332 3L38 9L32.332 15L31 13.59Z" fill="white"/></mask><g mask="url(#mask2)"><rect x="30.4426" y="2.25" width="7.55735" height="13" /></g></svg>'
     };
   }
 
@@ -193,7 +193,7 @@ export default class SimpleCarousel {
     if (captionText) {
       caption.value = captionText;
     }
-    caption.placeholder = 'Caption...';
+    caption.placeholder = this.config.captionPlaceholder;
 
     removeBtn.style.display = 'flex';
 
@@ -216,7 +216,7 @@ export default class SimpleCarousel {
       this._createImage(response.file.url, this.list.childNodes[this.list.childNodes.length - 2].firstChild, '', this.list.childNodes[this.list.childNodes.length - 2].firstChild.childNodes[1]);
       this.list.childNodes[this.list.childNodes.length - 2].firstChild.childNodes[2].style.backgroundImage = '';
       this.list.childNodes[this.list.childNodes.length - 2].firstChild.firstChild.value = response.file.url;
-      this.list.childNodes[this.list.childNodes.length - 2].firstChild.classList.add('carousel-item--empty');
+      this.list.childNodes[this.list.childNodes.length - 2].firstChild.classList.add('cdxcarousel-item--empty');
     } else {
       this.uploadingFailed('incorrect response: ' + JSON.stringify(response));
     }
